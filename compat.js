b81ecb4071deb5d6fe8dba3438b2687b207c5199
RoXoM <xyy94813@sina.com>
2019-04-11 16:36:07 +0800

    feat: throw error, and all errors as error message
    Why reject an error?
    https://eslint.org/docs/rules/prefer-promise-reject-errors
    
    resolve #1

--- compat.js
+++ compat.js
@@ -1,27 +1,26 @@
 var Relay = require('react-relay/compat');
 
 function commitMutationPromise(environment, config) {
-    return new Promise(function (resolve, reject) {
-        Relay.commitMutation(
-            environment,
-            Object.assign(
-                {},
-                config,
-                {
-                    onCompleted: function (payload, errors) {
-                        if (errors) {
-                            reject(errors[0]);
-                            return;
-                        }
-                        resolve(payload);
-                    },
-                    onError: function (error) {
-                        reject(error);
-                    }
-                }
-            )
-        );
-    });
+  return new Promise(function(resolve, reject) {
+    Relay.commitMutation(
+      environment,
+      Object.assign({}, config, {
+        onCompleted: function(payload, errors) {
+          if (errors && errors.length !== 0) {
+            const error = new Error(errors.toString());
+            error.payload = payload;
+            error.errors = errors;
+            reject(error); // or throw error
+            return;
+          }
+          resolve(payload);
+        },
+        onError: function(error) {
+          reject(error);
+        }
+      })
+    );
+  });
 }
 
 module.exports = commitMutationPromise;
